dc78021a516186d27a1880fb57a9e7cbe1d2cd09
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-12-01 23:13:17 +0530

    Added :UC3 Linked List is created using append method.

--- src/com/Bridgelabz/LinkedListCustom.java
+++ src/com/Bridgelabz/LinkedListCustom.java
@@ -43,11 +43,36 @@ public class LinkedListCustom<K> {
 			this.head = newNode;
 			this.head.setNext(tempNode);
 		
-			
 		}
-		
 }
-
+   public void append(INode newNode) {   
+		
+		  /*
+			 * head --> 56
+			 * 
+			 * head --> 56 --   --> null
+			 * 
+			 * head --> 56 --->30 --->null
+			 * 
+			 * head --> 56 ---> 30 ---  ---> null
+			 * 
+			 * head --> 56 ---> 30 ----> 70 ---> null
+			 * 
+			 */
+		  
+			if(this.tail == null) {
+				this.tail = newNode;
+			}
+			if(this.head == null) {
+				this.head = newNode;
+			}
+			else {
+				INode tempNode = this.tail;
+				this.tail.setNext(newNode); 
+				this.tail=newNode;
+			
+			}
+ }
 	public void dispalyLinkedList(){
 		System.out.println("My list"+head);
 	}
--- src/com/Bridgelabz/LinkedListMain.java
+++ src/com/Bridgelabz/LinkedListMain.java
@@ -8,13 +8,13 @@ public class LinkedListMain  {
 		MyNode<Integer> firstNode = new MyNode<Integer>(70);
 		MyNode<Integer> secondNode = new MyNode<Integer>(30);
 		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
-//		
-//		MyNode<Integer> fNode = new MyNode<Integer>(70);
-//		MyNode<Integer> sNode = new MyNode<Integer>(30);
-//		MyNode<Integer> tNode = new MyNode<Integer>(56);
-//		MyNode<Integer> fourthNode = new MyNode<Integer>(100);
-//		
-//		
+		
+		MyNode<Integer> fNode = new MyNode<Integer>(70);
+		MyNode<Integer> sNode = new MyNode<Integer>(30);
+		MyNode<Integer> tNode = new MyNode<Integer>(56);
+		MyNode<Integer> fourthNode = new MyNode<Integer>(100);
+		
+		
 		LinkedListCustom listnew1 = new LinkedListCustom();
 		listnew1.add(firstNode);
 		listnew1.add(secondNode);
@@ -22,6 +22,15 @@ public class LinkedListMain  {
 		
 		System.out.println("Linked List Add Method: ");
 		listnew1.dispalyLinkedList();
+		System.out.println("---------------------------------------------------------------------------------");
+		
+		LinkedListCustom listnew2 = new LinkedListCustom();
+		listnew2.append(fNode);
+		listnew2.append(sNode);
+		listnew2.append(tNode);
+		
+		System.out.println("\nLinked List Append Method: ");
+		listnew2.dispalyLinkedList();
 		System.out.println("---------------------------------------------------------------------------------");
 		
 		
